2ff6d071fe6d3448292dc958f0d31a93aa8d7ab6
C Lombard <stoflom@gmail.com>
2024-12-08 16:55:08 +0200

    Comments

--- src/app/services/persist.service.ts
+++ src/app/services/persist.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
-//This should use the database but for now we will use a cookie.
+//This should use the database or be stored statically here,
+// but for now we will use a cookie which allows the user the choice
+// of persistence across sesssions.
 
 @Injectable({
   providedIn: 'root'
