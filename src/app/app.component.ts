8610c259676c819e14178e85139ec105f6b2258a
Christoffel Lombard <stoflom@gmail.com>
2026-08-14 18:38:36 +0200

    fix: use inject() instead of constructor parameter injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessagesComponent } from './messages/messages.component';
 import { VersionService } from './services/version.service';
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   title = 'Dictionary of Names for Southern African Trees';
   version = '';
 
-  constructor(private versionService: VersionService) {}
+  private versionService = inject(VersionService);
 
   async ngOnInit(): Promise<void> {
     this.version = await this.versionService.getVersion();
